02211588d59ebe740c696c031794cbd46148f1d4
fots <facundotomatis@gmail.com>
2022-08-20 18:31:55 -0300

    now login and logout show whenever necessary

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from 'src/app/service/auth.service'
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  isLogged(): Boolean {
+    return this.authService.loggedIn()
   }
 
-  public isMenuCollapsed = true;
+  logOut() {
+    this.authService.logOut()
+  }
 }
