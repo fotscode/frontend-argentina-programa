b52e564e60c69a28ea6d07624ce74cb2b788c2ce
fots <facundotomatis@gmail.com>
2022-08-21 20:50:03 -0300

    added service for experience

---
+++ src/app/service/experience.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Experience } from '../interface/experience';
+import { ExperienceResponse } from '../interface/experience-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienceService {
+  private readonly apiUrl = `${environment.baseApiUrl}/experience`
+
+  constructor(private http:HttpClient) { }
+
+  getExperiences(): Observable<ExperienceResponse> {
+    return this.http.get<ExperienceResponse>(`${this.apiUrl}/list`);
+  }
+
+  getExperience(id: number): Observable<ExperienceResponse>{
+    return this.http.get<ExperienceResponse>(`${this.apiUrl}/get/${id}`);
+  }
+
+  updateExperience(experience:Experience): Observable<ExperienceResponse> {
+    return this.http.post<ExperienceResponse>(`${this.apiUrl}/update`,experience);
+  }
+
+  createExperience(experience:Experience): Observable<ExperienceResponse> {
+    return this.http.post<ExperienceResponse>(`${this.apiUrl}/save`,experience);
+  }
+
+  deleteExperience(id: number): Observable<ExperienceResponse>{
+    return this.http.delete<ExperienceResponse>(`${this.apiUrl}/delete/${id}`);
+  }
+}
