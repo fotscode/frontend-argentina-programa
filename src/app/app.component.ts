a9d0a431aced0d0fb101106bcf7b511b5b11f880
fots <facundotomatis@gmail.com>
2022-08-23 14:25:30 -0300

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private title:Title){
+    this.title.setTitle("Facundo Tomatis | Portfolio")
+  }
+
+}
